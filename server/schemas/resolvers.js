e83d46612da6c2d67352425d32e070c267356ff9
trujilml <mickeytrj@gmail.com>
2022-02-16 10:00:49 -0600

    started on schemas directory - completed index file and working on resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,13 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Book } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+            }
+        }
+    }
+}

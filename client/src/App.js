241dcf17ccd42c58be1bcea8cbd4137c195ffbb9
trujilml <mickeytrj@gmail.com>
2022-02-17 13:51:50 -0600

    updated client files

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
   // uri: 'http://localhost:3001/graphql'
-  request: operation => {
+  request: (operation) => {
     const token = localStorage.getItem('id_token');
 
     operation.setContext({
       headers: {
         authorization: token ? `Bearer ${token}` : ''
-      }
-    })
+      },
+    });
   },
-  uri: '/graphql'
+
+  uri: '/graphql',
 });
 
 

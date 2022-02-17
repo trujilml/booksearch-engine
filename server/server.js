b1c1612760e5d6db13f6349967d06eb962d5887b
trujilml <mickeytrj@gmail.com>
2022-02-17 13:27:38 -0600

    updated server file - checking to see connection problem with apollo graphql

--- server/server.js
+++ server/server.js
@@ -3,10 +3,9 @@ const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 
 const path = require('path');
-
 const db = require('./config/connection');
-const routes = require('./routes');
 
+// const routes = require('./routes');
 
 //import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
@@ -28,28 +27,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
+// app.use(routes);
 
-
-app.use(routes); //comment this out in the end
-
-
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-
   });
 });
 
-process.on('uncaughtException', function(err) {
-  console.log('Caught exception: ' + err);
+db.on("error", (err) => {
+  console.error("MongoDB connection error: ", err);
 });

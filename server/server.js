40f18a998eba49b9fb41f73e83f05247bcafde67
trujilml <mickeytrj@gmail.com>
2022-02-17 21:10:06 -0600

    updated server file - errors still running on client file

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on localhost:${PORT}!`);
     // log where we can go to test our GQL API
-    // console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 
  });
